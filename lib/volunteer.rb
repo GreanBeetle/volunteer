44eb9353042ad492c99394caa142d0c18b864257
John Rykken <john.rykken@gmail.com>
2018-02-23 09:40:13 -0800

    Pass test 12. Save volunteer. Write volunteer self.all method.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -21,4 +21,21 @@ class Volunteer
     self.name.==(new_volunteer.name)
   end
 
+  def save
+    var = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
+    @id = var.first.fetch('id').to_i
+  end
+
+  def self.all
+    db_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    db_volunteers.each do |volunteer|
+      name = volunteer.fetch('name')
+      project_id = volunteer.fetch('project_id').to_i
+      id = volunteer.fetch('id').to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    return volunteers
+  end
+
 end # Volunteer
