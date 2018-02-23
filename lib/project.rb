542b252b44ee39a89bec30c7015a7e84b6c1b687
John Rykken <john.rykken@gmail.com>
2018-02-23 08:43:03 -0800

    Fix Gemfile error. Pass Project test 1. Return project title.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Project
+  attr_accessor :title, :id, :volunteer_id
+
+  def initialize(attributes)
+    @title = attributes[:title]
+    @id = attributes[:id]
+  end
+
+  def title
+    @title
+  end
+
+end # project
